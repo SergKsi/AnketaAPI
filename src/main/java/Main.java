9f67d369b4492c6f4097b7fd2cabd110e1dd7205
S.Kosovichev <sergksi@mail.ru>
2022-08-30 20:51:29 +0300

    Second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.birthday.day = 10;
+        post.birthday.month = 7;
+        post.birthday.year = 1999;
+
+        post.name = "Иван";
+        post.passport = "1234 N 123456";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+    }
+}
